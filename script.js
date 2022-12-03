84d1f601391a00ba0d624bf4d1b159d670cb69b0
vominht <103721562+vominht@users.noreply.github.com>
2022-12-03 12:05:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -22,28 +22,28 @@ let updateTimer;
 
 const music_list = [
     {
-        img : 'images/stay.png',
+        img : 'stay.png',
         name : 'Stay',
         artist : 'The Kid LAROI, Justin Bieber',
-        music : 'music/stay.mp3'
+        music : 'stay.mp3'
     },
     {
-        img : 'images/fallingdown.jpg',
+        img : 'fallingdown.jpg',
         name : 'Falling Down',
         artist : 'Wid Cards',
-        music : 'music/fallingdown.mp3'
+        music : 'fallingdown.mp3'
     },
     {
-        img : 'images/faded.png',
+        img : 'faded.png',
         name : 'Faded',
         artist : 'Alan Walker',
-        music : 'music/Faded.mp3'
+        music : 'Faded.mp3'
     },
     {
-        img : 'images/ratherbe.jpg',
+        img : 'ratherbe.jpg',
         name : 'Rather Be',
         artist : 'Clean Bandit',
-        music : 'music/Rather Be.mp3'
+        music : 'Rather Be.mp3'
     }
 ];
 
